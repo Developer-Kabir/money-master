4e5137c6a16f4549898317c6923883e102eae963
Developer-Kabir <hakabir40@gmail.com>
2022-02-17 07:22:41 +0600

    error handle update

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-document.getElementById("calculate-btn").addEventListener("click", function(){
+// balance calculation function 
+
+function balanceCalculation(){
     const incomeInput = document.getElementById("Income-input");
     const income = parseInt(incomeInput.value) ;
     const foodInput = document.getElementById("food-input");
@@ -8,29 +10,67 @@ document.getElementById("calculate-btn").addEventListener("click", function(){
     const clothesInput = document.getElementById("clothes-input");
     const clothesCost = parseInt(clothesInput.value);
     let totalCost = foodCost + rentCost + clothesCost ;
+    const balance = income - totalCost ;
 
-    
+    if (income < 0 || foodCost < 0 || rentCost < 0 || clothesCost < 0 ){
+        window.alert("Please input positive value");
+    }
+    else {
+        if ( totalCost > income ){
+            window.alert("You can not expense more than of your income. Please decrease your cost");
+        }
+        else {
+            let totalExpense =  document.getElementById("total-expense");
+            totalExpense.innerText = totalCost;
+            const balenceText = document.getElementById("balance");
+            balenceText.innerText = balance;
+        }
+    }
+}
 
-    let totalExpense =  document.getElementById("total-expense");
-    totalExpense.innerText = totalCost;
+//  remaining balance calculation functionb 
 
-     balance = income - totalCost ;
-    const balenceText = document.getElementById("balance");
-    balenceText.innerText = balance;
-       
-})
-
-document.getElementById("saving-btn").addEventListener("click", function(){
+function remainingBalenceCalculation(){
     const savingInput = document.getElementById("saving-input");
     const savingRatio = parseInt(savingInput.value);
     const incomeInput = document.getElementById("Income-input");
     const income = parseInt(incomeInput.value) ;
+    const foodInput = document.getElementById("food-input");
+    const foodCost = parseInt(foodInput.value) ;
+    const rentInput = document.getElementById("rent-input");
+    const rentCost = parseInt(rentInput.value) ;
+    const clothesInput = document.getElementById("clothes-input");
+    const clothesCost = parseInt(clothesInput.value);
+    let totalCost = foodCost + rentCost + clothesCost ;
+    const balance = income - totalCost ;
     const savingAmount = (income * savingRatio) / 100 ;
-    const savingAmountText = document.getElementById("saving-amount");
-    savingAmountText.innerText = savingAmount ;
+     
+    if ( savingRatio < 0){
+        window.alert("Please Input Positive value for saving. Negative value is not accepted")
+    }
+    else{
+        if ( savingAmount > balance){
+            window.alert("You can not save more than of your balence")
+        }
+        else{
+            const savingAmountText = document.getElementById("saving-amount");
+            savingAmountText.innerText = savingAmount ;
 
-    const remainingBalence = balance - savingAmount ;
-    const remainingBalenceText = document.getElementById("remaining-balance");
-    remainingBalenceText.innerText = remainingBalence;
-    
+            const remainingBalence = balance - savingAmount ;
+            const remainingBalenceText = document.getElementById("remaining-balance");
+            remainingBalenceText.innerText = remainingBalence;
+        }
+    }
+}
+
+//  handel even for click claculate button 
+document.getElementById("calculate-btn").addEventListener("click", function(){
+    balanceCalculation();
+       
 })
+
+//  handel event for save button 
+document.getElementById("saving-btn").addEventListener("click", function(){
+    remainingBalenceCalculation();
+})
+
